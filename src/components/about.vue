14eca0dafaf955741048cd89c41b331b97d0018b
Sarah Cohen <cohen730@umn.edu>
2024-12-04 16:53:49 -0600

    Added about page photo, made new inputs for catering page

--- src/components/about.vue
+++ src/components/about.vue
@@ -6,14 +6,25 @@ import menu from "../restaurant_menu.js"
 
 <template>
   <primary-template>
-    <h1>This is my About page</h1>
+<!--    <h1>This is my About page</h1>-->
 
 <!--    <div v-for="(item, index) in menu.items" :key="index">-->
 <!--      {{ item.name}}, {{ item.price }}, {{item.description}}, {{item.ingredients}}-->
 <!--    </div>-->
+    <img src="../assets/family.jpg" alt="Family">
+    <h2>Who we are</h2>
+
   </primary-template>
 </template>
 
 <style scoped>
+h2 {
+  text-align: center;
+}
+
+img {
+  width: 1000px;
+  height: 600px;
+}
 
 </style>
--- src/components/catering.vue
+++ src/components/catering.vue
@@ -10,25 +10,41 @@ import Hero from "../components/Hero.vue";
        sub-head="Let us cater your special events and celebrations!"
  />
 
+<h4>Please fill the form to contact with any questions you may have.</h4>
 
   <div class="container">
     <form action="action_page.php">
 
-      <label for="fname">First Name</label>
-      <input type="text" id="fname" name="firstname" placeholder="Your name..">
+      <label for="name">Name</label>
+      <input type="text" id="name" name="name" placeholder="Your first and last name..">
 
-      <label for="lname">Last Name</label>
-      <input type="text" id="lname" name="lastname" placeholder="Your last name..">
+      <label for="phone">Phone</label>
+      <input type="text" id="phone" name="phone-number" placeholder="Enter the best number to contact you..">
 
-      <label for="country">Country</label>
-      <select id="country" name="country">
-        <option value="australia">Australia</option>
-        <option value="canada">Canada</option>
-        <option value="usa">USA</option>
-      </select>
+      <label for="email">Email</label>
+      <input type="text" id="email" name="email" placeholder="Your email..">
 
-      <label for="subject">Subject</label>
-      <textarea id="subject" name="subject" placeholder="Write something.." style="height:200px"></textarea>
+      <label for="event-type">Event Type</label>
+      <input type="text" id="event-type" name="event-type" placeholder="Enter your event type (e.g., birthday, corporate meeting)..">
+
+      <label for="date">Date</label>
+      <input type="text" id="date" name="date" placeholder="Date of event..">
+
+      <label for="guest-count">Guest Count</label>
+      <input type="text" id="guest-count" name="guest-count" placeholder="How many guests will be attending?">
+
+      <label for="delivery-details">Delivery Details</label>
+      <input type="text" id="delivery-details" name="delivery-details" placeholder="Enter your delivery address and preferred time..">
+
+<!--      <label for="country">Country</label>-->
+<!--      <select id="country" name="country">-->
+<!--        <option value="australia">Australia</option>-->
+<!--        <option value="canada">Canada</option>-->
+<!--        <option value="usa">USA</option>-->
+<!--      </select>-->
+
+      <label for="message">Message</label>
+      <textarea id="message" name="message" placeholder="Tell us what items you would like .." style="height:200px"></textarea>
 
       <input type="submit" value="Submit">
 
@@ -42,32 +58,34 @@ input[type=text], select, textarea {
   width: 100%; /* Full width */
   padding: 12px; /* Some padding */
   border: 1px solid #ccc; /* Gray border */
-  border-radius: 4px; /* Rounded borders */
+  border-radius: 10px; /* Rounded borders */
   box-sizing: border-box; /* Make sure that padding and width stays in place */
   margin-top: 6px; /* Add a top margin */
   margin-bottom: 16px; /* Bottom margin */
-  resize: vertical /* Allow the user to vertically resize the textarea (not horizontally) */
+  resize: vertical;/* Allow the user to vertically resize the textarea (not horizontally) */
+  font-family: Avenir, sans-serif;
+  font-size: 1rem;
 }
 
 /* Style the submit button with a specific background color etc */
 input[type=submit] {
-  background-color: #04AA6D;
+  background-color: #EA9637;
   color: white;
-  padding: 12px 20px;
+  padding: 5px 10px;
   border: none;
-  border-radius: 4px;
+  border-radius: 10px;
   cursor: pointer;
 }
 
 /* When moving the mouse over the submit button, add a darker green color */
 input[type=submit]:hover {
-  background-color: #45a049;
+  background-color: #BD6500;
 }
 
 /* Add a background color and some padding around the form */
 .container {
   border-radius: 5px;
-  background-color: #f2f2f2;
+  background-color: #11120C;
   padding: 20px;
 }
 
